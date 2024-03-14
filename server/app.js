6e66cffaae08a465d033519a95b0f8f5dccd4bee
Induranga Kawishwara <induranga.20200688@iit.ac.lk>
2024-03-14 09:24:03 +0530

    update

--- server/app.js
+++ server/app.js
@@ -9,6 +9,11 @@ const PORT = 6000;
 app.use(cors());
 app.use(express.json());
 
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.error("MongoDB connection error:", err));
+
 app.listen(PORT, () => {
   console.log(`Server running on port: http://localhost:${PORT}`);
 });
