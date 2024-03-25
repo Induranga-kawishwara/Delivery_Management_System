3683879101ef35d637bb0ce90ab53fcab2b62515
Induranga Kawishwara <induranga.20200688@iit.ac.lk>
2024-03-25 10:35:17 +0530

    update

--- gui/src/components/Cart/Home.js
+++ gui/src/components/Cart/Home.js
@@ -1,47 +1,38 @@
-import React from 'react';
-import axios from 'axios'; 
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import Itemcard from "./Itemcard";
-import data from "./data";
-import './Home.css'
+// import data from "./data";
+import "./Home.css";
 
 const Home = () => {
-    const sendProductData = async (item) => { 
-        try {
-            const response = await axios.post('/products', {
-                img: item.img,
-                title: item.title,
-                desc: item.desc,
-                price: item.price
-            });
-            console.log(response.data);
-        } catch (error) {
-            console.error('Error:', error);
-        }
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      console.log("fuck");
+      try {
+        const response = await axios.get("http://localhost:5000/product");
+        setData(response.data);
+        console.log(response.data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
     };
+    fetchData();
+  }, []);
 
-    return (
-        <div> 
-            <h1 className="text-center mt-3">All Items</h1>   
-            <section className="py-4 container">
-                <div className="row justify-content-center">
-                    {data.productData.map((item, index) => (
-                        <Itemcard 
-                            img={item.img} 
-                            title={item.title} 
-                            desc={item.desc} 
-                            price={item.price} 
-                            item={item} 
-                            key={index}
-                            sendProductData={() => sendProductData(item)} 
-                        />
-                    ))}
-                </div>
-            </section>
-            
-
+  return (
+    <div>
+      <h1 className="text-center mt-3">All Items</h1>
+      <section className="py-4 container">
+        <div className="row justify-content-center">
+          {data.map((item, index) => (
+            <Itemcard data={item} key={index} />
+          ))}
         </div>
-                  
-    );
+      </section>
+    </div>
+  );
 };
 
 export default Home;
--- gui/src/components/Cart/Itemcard.js
+++ gui/src/components/Cart/Itemcard.js
@@ -1,74 +1,107 @@
-import React, { useState } from 'react';
-import { useCart } from 'react-use-cart';
-import './itemcard.css'
+import React, { useEffect, useState } from "react";
+import { useCart } from "react-use-cart";
+import "./itemcard.css";
+import axios from "axios";
 
 const ItemCard = (props) => {
-  const { addItem } = useCart();
-  const [selectedSize, setSelectedSize] = useState('50g');
+  const { addItem, emptyCart } = useCart();
+  const [sizes, setSizes] = useState([{ size: "", shopPrice: "" }]);
+  const [selectedSize, setSelectedSize] = useState({ size: "", shopPrice: "" });
+  const [cart, setCart] = useState([{}]);
 
-  const prices = {
-    'Curry Powder': {
-      
-      '25g': 32.50,
-      '50g': 75,
-      '100g': 150,
-      '500g': 650,
-      '1kg': 1200
-    },
-    'Chilli Powder': {
-      '25g': 45,
-      '50g': 90,   
-      '100g': 180, 
-      '500g': 700, 
-      '1kg': 1250  
-    },
-    'Chilli Pieces': {
-      '25g': 45,
-      '50g': 90,
-      '100g': 180,
-      '500g': 700,
-      '1kg': 1250
-    },
-    'Tumeric Powder': {
-      '25g': 75,
-      '50g': 150,
-      '100g': 300,
-      '500g': 950,
-      '1kg': 1900
-    },
-    'Peppar Powder': {
-      '25g': 85,
-      '50g': 170,
-      '100g': 340,
-      '500g': 1300,
-      '1kg': 2600
+  const fetchCart = async () => {
+    try {
+      const response = await axios.get(
+        `http://localhost:5000/cart/${localStorage.getItem("userId")}`
+      );
+      setCart(response.data);
+      console.log(cart);
+    } catch (error) {
+      console.error("Error fetching cart:", error);
+      throw error;
     }
   };
+  const preAddToCart = () => {
+    console.log("1");
 
-  const handleAddToCart = () => {
-    const price = prices[props.title][selectedSize]; // Get the price based on product title and selected size
-    addItem({
-      ...props.item,
-      size: selectedSize,
-      price: price,
-      id: props.item.id + '-' + selectedSize // Unique identifier for each combination
+    cart.forEach((item) => {
+      console.log("2");
+      addItem({
+        img: item.details.img,
+        title: item.details.title,
+        size: item.details.size,
+        price: item.details.price,
+        id: item.details.id,
+        quantity: item.details.quantity,
+      });
     });
   };
 
-  const handleSelectSize = (size) => {
-    setSelectedSize(size);
+  useEffect(() => {
+    emptyCart();
+    fetchCart();
+    console.log("ss");
+    console.log(cart);
+
+    if (!cart) {
+      preAddToCart();
+    }
+
+    const extractedSizes = props.data.sizes.map((item) => ({
+      size: item.size,
+      shopPrice: item.shopPrice,
+    }));
+    setSizes(extractedSizes);
+    const lowestPriceSize = extractedSizes.reduce((prev, current) =>
+      prev.price < current.price ? prev : current
+    );
+    setSelectedSize(lowestPriceSize);
+  }, [props.data.sizes]);
+
+  const handleAddToCart = async () => {
+    addItem({
+      img: props.data.image,
+      title: props.data.productName,
+      size: selectedSize.size,
+      price: selectedSize.shopPrice,
+      id: props.data._id + "-" + selectedSize.size,
+    });
+    const productData = {
+      id: localStorage.getItem("riderId"),
+      title: props.data.productName,
+      size: selectedSize.size,
+      price: selectedSize.shopPrice,
+      image: props.data.image,
+      productid: props.data._id + "-" + selectedSize.size,
+    };
+    try {
+      const response = await axios.post(
+        "http://localhost:5000/cart",
+        productData
+      );
+      console.log(response.data);
+    } catch (error) {
+      console.error("Error adding product to cart:", error);
+    }
+  };
+
+  const handleSelectSize = (size, price) => {
+    setSelectedSize({ size: size, shopPrice: price });
   };
 
   return (
+    // <></>
     <div className="col-11 col-md-6 col-lg-3 mx-0 mb-4">
       <div className="card p-0 overflow-hidden h-100 shadow">
-
-
-        <img src={props.img} className="card-img-top img-fluid" alt={props.title} />
+        <img
+          src={props.data.image}
+          className="card-img-top img-fluid"
+          alt={props.data.productName}
+        />
         <div className="card-body text-center">
-          <h5 className="card-title">{props.title}</h5>
-          
-                    <div className="dropdown">
+          <h5 className="card-title">{props.data.productName}</h5>
+
+          <div className="dropdown">
             <button
               className="btn btn-secondary dropdown-toggle"
               type="button"
@@ -77,46 +110,25 @@ const ItemCard = (props) => {
               aria-haspopup="true"
               aria-expanded="false"
             >
-              {selectedSize}
+              {selectedSize.size}
             </button>
-            <div className="custom-dropdown-menu" aria-labelledby="dropdownMenuButton">
-            <button
-                className="custom-dropdown-item"
-                onClick={() => handleSelectSize('25g')}
-              >
-                25g
-              </button>              
-              <button
-                className="custom-dropdown-item"
-                onClick={() => handleSelectSize('50g')}
-              >
-                50g
-              </button>
-              <button
-                className="custom-dropdown-item"
-                onClick={() => handleSelectSize('100g')}
-              >
-                100g
-              </button>
-              <button
-                className="custom-dropdown-item"
-                onClick={() => handleSelectSize('500g')}
-              >
-                500g
-              </button>
-              <button
-                className="custom-dropdown-item"
-                onClick={() => handleSelectSize('1kg')}
-              >
-                1kg
-              </button>
+            <div
+              className="custom-dropdown-menu"
+              aria-labelledby="dropdownMenuButton"
+            >
+              {sizes.map((item, indx) => (
+                <button
+                  key={indx}
+                  className="custom-dropdown-item"
+                  onClick={() => handleSelectSize(item.size, item.shopPrice)}
+                >
+                  {item.size}
+                </button>
+              ))}
             </div>
           </div>
 
-
-
-          <h5 className="card-title">Rs. {prices[props.title][selectedSize]}</h5>
-          <p className="card-text">{props.desc}</p>
+          <h5 className="card-title">Rs. {selectedSize.shopPrice}</h5>
           <button className="btn btn-success" onClick={handleAddToCart}>
             Add to Cart
           </button>
